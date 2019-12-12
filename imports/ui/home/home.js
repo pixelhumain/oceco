32440636234dabeb210cb16fd358ab102bc14d58
Thibaut lefort <cremoi974@gmail.com>
2019-12-12 14:10:32 +0400

    première version trie

--- imports/ui/home/home.js
+++ imports/ui/home/home.js
@@ -11,11 +11,13 @@ import { Events } from '../../api/events.js';
 import { Organizations } from '../../api/organizations.js';
 import { Projects } from '../../api/projects.js';
 import { Citoyens } from '../../api/citoyens.js';
+import { Actions } from '../../api/actions';
 
 window.Events = Events;
 window.Organizations = Organizations;
 window.Projects = Projects;
 window.Citoyens = Citoyens;
+window.Actions = Actions;
 
 Template.home.onCreated(function(){
     Meteor.subscribe('notificationsUser');
@@ -23,4 +25,22 @@ Template.home.onCreated(function(){
     this.subscribe('scopeDetail', 'organizations', '5de9df6d064fca0d008b4568');
     this.subscribe('directoryList', 'organizations', '5de9df6d064fca0d008b4568');
     this.subscribe('directoryListProjects', 'organizations', '5de9df6d064fca0d008b4568');
+    this.sortByDate = new ReactiveVar (false)
+})
+
+Template.home.events({
+    'click #sortByDate '(event, instance){
+        console.log('YO')
+        Template.instance().sortByDate.set(true)
+    }
+       
+})
+
+Template.home.helpers({
+    projectAction(){
+        if (Template.instance().sortByDate.get()) {
+            return Actions.find({}, {sort: {startDate: -1}})
+        }
+        return Actions.find()
+    },
 })
