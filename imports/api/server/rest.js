aa6306b516828c949860e1619f9f2c9d3f221d78
aboire <thomas.craipeau@gmail.com>
2020-06-20 11:01:41 +0400

    ajout synchro admin rest

--- imports/api/server/rest.js
+++ imports/api/server/rest.js
@@ -1,10 +1,16 @@
 /* eslint-disable consistent-return */
 /* eslint-disable no-underscore-dangle */
-/* eslint-disable no-undef */
 import express from 'express';
 import { Meteor } from 'meteor/meteor';
 import { WebApp } from 'meteor/webapp';
+import { Random } from 'meteor/random';
+import { DDPCommon } from 'meteor/ddp-common';
+import { DDP } from 'meteor/ddp';
+import { Mongo } from 'meteor/mongo';
 import bodyParser from 'body-parser';
+import { Organizations } from '../organizations.js';
+import { Projects } from '../projects.js';
+import { nameToCollection } from '../helpers.js';
 import { SchemasActionsRest } from '../actions.js';
 
 function verifyToken(req, res, next) {
@@ -28,7 +34,7 @@ function verifyToken(req, res, next) {
 }
 
 const runAsUser = function (userId, func) {
-  const DDPCommon = Package['ddp-common'].DDPCommon;
+  // const DDPCommon = Package['ddp-common'].DDPCommon;
   // put whatever you want in these properties
   const invocation = new DDPCommon.MethodInvocation({
     isSimulation: false,
@@ -67,6 +73,34 @@ const handleErrorAsJson = function (err, req, res) {
   return res.status(err.statusCode || 500).json(body);
 };
 
+const synchroAdmin = function ({ parentId, parentType }) {
+  const collectionScope = nameToCollection(parentType);
+  const scopeOne = collectionScope.findOne({
+    _id: new Mongo.ObjectID(parentId),
+  });
+
+  if (scopeOne) {
+    if (parentType === 'organizatOrganizationsions') {
+      const testConnectAdminRetour = Meteor.call('testConnectAdmin', { id: scopeOne._id._str });
+      return testConnectAdminRetour;
+    } else if (parentType === 'projects') {
+      const project = `links.projects.${scopeOne._id._str}`;
+      const organizationOne = Organizations.findOne({ [project]: { $exists: 1 }, oceco: { $exists: 1 } });
+      const testConnectAdminRetour = Meteor.call('testConnectAdmin', { id: organizationOne._id._str });
+      return testConnectAdminRetour;
+    } else if (parentType === 'events') {
+      const event = `links.events.${scopeOne._id._str}`;
+      const projectOne = Projects.findOne({ [event]: { $exists: 1 } });
+      if (projectOne) {
+        const project = `links.projects.${projectOne._id._str}`;
+        const organizationOne = Organizations.findOne({ [project]: { $exists: 1 }, oceco: { $exists: 1 } });
+        const testConnectAdminRetour = Meteor.call('testConnectAdmin', { id: organizationOne._id._str });
+        return testConnectAdminRetour;
+      }
+    }
+  }
+};
+
 const app = express();
 WebApp.connectHandlers.use(bodyParser.json());
 WebApp.connectHandlers.use(bodyParser.urlencoded({ extended: false }));
@@ -78,6 +112,9 @@ app.post('/api/action/create', verifyToken, function (req, res) {
   runAsUser(userId, function () {
     try {
       const actionPost = SchemasActionsRest.clean(req.body);
+
+      const synchroRetour = synchroAdmin({ parentId: req.body.parentId, parentType: req.body.parentType });
+
       const retourCall = Meteor.call('insertAction', actionPost);
       // console.log(retourCall);
       const valid = {
